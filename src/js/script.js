99c9f2693af360d2b4b3ca58295aaae4c4bc9750
ortamfag <a_zhidov@outlook.com>
2022-04-14 17:25:17 +0300

    checkpoint before change coordinates

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,8 @@ const secondSlide = document.querySelector('.second-slide')
 const st1 = document.querySelector('.st1-text')
 const st2 = document.querySelector('.st2')
 const st3 = document.querySelector('.st3')
+const st4 = document.querySelector('.st4')
+const st5 = document.querySelector('.st5')
 const leftBeacon = document.querySelector('#left-beacon')
 
 var counter = 0
@@ -22,6 +24,8 @@ function Slides(){
         secondSlide.classList.add('second-slide-show')
         st2.classList.add('st2-show')
         st3.classList.add('st3-show')
+        st4.classList.add('st4-show')
+        st5.classList.add('st5-show')
     }
 
 
@@ -31,6 +35,9 @@ function Slides(){
         st1.classList.remove('st1-text-show')
         secondSlide.classList.remove('second-slide-show')
         st2.classList.remove('st2-show')
+        st3.classList.remove('st3-show')
+        st4.classList.remove('st4-show')
+        st5.classList.remove('st5-show')
     }  
 
     if (counter === 5) {
